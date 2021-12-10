e4d501e648587081e9c61a2bd69a972d354cee36
unknown <cfergus@cnm.edu>
2021-12-10 03:51:58 -0700

    pushes not working because of media files

---
+++ HydroCode/src/credentials.h
@@ -0,0 +1,5 @@
+/************************* Adafruit.io Setup *********************************/ 
+#define AIO_SERVER      "io.adafruit.com" 
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL 
+#define AIO_USERNAME  "drteeth"
+#define AIO_KEY       "aio_jOsv39SFcNcef5ybzUYDoSZijL37"
---
+++ HydroCode/target/3.1.0/argon/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+c\:/Users/School/Documents/IoT/Hydroponic-Capstone/sensorTest/target/3.1.0/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
